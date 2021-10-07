4d958d57fb510a524570cc97eaf380413de66ea6
Julaxe <julaxe8@gmail.com>
2021-10-07 19:09:02 -0400

    Excercise 3 done

--- Labs/Assets/Scripts/CharacterMovement.cs
+++ Labs/Assets/Scripts/CharacterMovement.cs
@@ -11,9 +11,14 @@ public class CharacterMovement : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private bool onGrass = false;
+
+    private BattleManager battleManager;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        battleManager = GameObject.FindObjectOfType<BattleManager>();
     }
 
     // Update is called once per frame
@@ -23,5 +28,32 @@ public class CharacterMovement : MonoBehaviour
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
         rb.velocity = new Vector2(inputX, inputY) * Fspeed * Time.fixedDeltaTime;
+        if (onGrass)
+        {
+            if(rb.velocity != Vector2.zero) //if I'm moving
+            {
+                if(Random.Range(0,100) == 0) // 1/50 chance every frame to encounter a battle
+                {
+                    battleManager.StartBattle();
+                }
+            }
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            onGrass = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            onGrass = false;
+        }
+    }
+
 }
